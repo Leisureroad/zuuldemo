2b992cddfa0fe30b28fe1fb18c420af5ecb8d0ac
Fan Liu <charles_liuf@hotmail.com>
2018-06-08 20:27:03 +0800

    提交zuuldemo

---
+++ src/main/java/com/example/zuuldemo/ZuuldemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.zuuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuuldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuuldemoApplication.class, args);
+	}
+}
